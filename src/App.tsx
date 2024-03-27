b5ab76dc1a8f18f0c797f5a2645cf1be7de098f0
unknown <williamkhayashi@gmail.com>
2024-03-26 21:02:59 -0300

    Atualização da page app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
+import Header from '../src/header/header';
+import Body from './body/body';
+import Formulario from './formulario/formulario';
+import Footer from './footer/footer';
 import './App.css'; 
 
 function App() {
     return (
         <>
-            
+            <Header />
+            <div className="container">
+                <Body />
+                <Formulario />
+            </div>
+            <Footer />
         </>
     );
 }
